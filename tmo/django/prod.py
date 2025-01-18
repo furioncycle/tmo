b2d7b0fa40b58f7193f82fc2076426f542e763e7
furioncycle <ttecho2021@gmail.com>
2025-01-17 18:34:46 -0800

    prod update

--- tmo/django/prod.py
+++ tmo/django/prod.py
@@ -1,9 +1,14 @@
+import json, os, sys
 from .base import *
 from tmo.env import env
 
 DEBUG = env.bool("DJANGO_DEBUG", default=False)
 
 ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", default=[])
+
+creds = json.loads(os.getenv('VCAP_SERVICES'))['user-provided'][0]['credentials']
+
+SECRET_KEY = creds['SECRET_KEY']
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
